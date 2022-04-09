e135ab118129dc99dd961d729b05ab76fc467e02
Alex Gonzaga <alex@nbp-168-240.nbp.ruw.rutgers.edu>
2022-04-08 20:54:36 -0400

    Wordle 1.5.2

---
+++ resourcesTemplate.py
@@ -0,0 +1,26 @@
+TOKEN = "Discord Bot Token"
+#put your discord bot token here, but keep it a secret!
+
+
+alphaVantageKey='Alpha Vantage API for if you need want live stock data'
+#put your alpha vantage key there
+
+
+message60 = "You failed the test!"
+
+message70 = "oops! you didn't fail but you need to do a bit better next time"
+
+message80 = "almost there, but you're doing alright!"
+
+message90 = "Nice!""
+
+message100 = "Great Job! You got an A!"
+# random messages for the test program
+
+
+greetings = ['hello!',
+             'Isn\'t it lovely weather we\'re having?',
+             'Good Day, everyone!',
+             'HI!',
+             'Hello, world!']
+#greetins for !greet
